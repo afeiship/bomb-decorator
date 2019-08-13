a10d0ed43957f245a86409704056c179e77660cb
afeiship <1290657123@qq.com>
2019-08-13 17:38:08 +0800

    add: index/key support

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var COMMA = ',';
 
-module.exports = function (inTarget, inKey, inDescriptor) {
+module.exports = function(inTarget, inKey, inDescriptor) {
   var method = inDescriptor.value;
-  if(inKey.indexOf(COMMA)>-1){
+  if (inKey.indexOf(COMMA) > -1) {
     var keys = inKey.split(COMMA);
-    keys.forEach(function(key){
-        inTarget[ key ] = method.call( inTarget, key );
+    keys.forEach(function(key, index) {
+      inTarget[key] = method.call(inTarget, key, index);
     });
   }
 };
